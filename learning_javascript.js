a614061c77a18803de81e0ece0ae92fddbfc2a7b
lunaticphy <132862878+lunaticphy@users.noreply.github.com>
2023-06-08 20:57:25 -0500

    Create learning_javascript.js

---
+++ learning_javascript.js
@@ -0,0 +1,18 @@
+var name = "John"
+var name_2 = "Mike"
+let num = 4
+
+console.log("Hello", name, "and", name_2);
+console.log(1 + num); //summation with variable 
+console.log(1 - 2) // subtraction 
+console.log(1 * 2) // multiplication
+console.log(1 / 2) // division 
+//final analysis on var and console.log()
+let x = 7
+let y = 8
+var sumation_x_y = x + y
+console.log("Sumation when x= 7 and y= 8", sumation_x_y);
+//now user input
+//user input is caractarised by prompt
+var user_name = prompt("What is your name ");
+console.log("Hi", user_name);
